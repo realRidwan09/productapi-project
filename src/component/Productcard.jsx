85e3694b262fa79e556f945f9c53517cd2f8faf6
realRidwan09 <162730097+realRidwan09@users.noreply.github.com>
2024-04-11 09:54:43 -0700

    new

--- src/component/Productcard.jsx
+++ src/component/Productcard.jsx
@@ -1,24 +1,23 @@
-import React from "react"
-import {Link} from 'react-router-dom'
+import React from 'react'
+import './Productcard.css'
+import { Link } from 'react-router-dom'
 
-const Productcard = (props)=>{
-    return(
-        <>
-            <div className="parentWrap">
-                <div id="productDetails">
-                    <h3 id="productName">{props.title}</h3>
-                    <h4 id="productPrice">{props.price}</h4>
-                    <p id="productDescription">{props.description}</p>
-                    <p id="productCatgories">{props.category}</p>
-                </div>
-                <img id="productImage" src={props.image} alt="productImage" />
+const Productcard = (props) => {
+  return (
+    <section className='productCard-wrap'>
+        <div>
+            <h3 id='productTitle'>{props.title}</h3>
+            <h4 id='productPrice'>Price: ${props.price}</h4>
+            <p id='productDescription'><b>Description:</b> {props.description}</p>
+            <p id='productCategory'><b>Category:</b> {props.category}</p>
+        </div>
+        <img id='productImage' src={props.image} alt="productImage" />
 
-                {/* <Link to ={'/productPage'}>
-                    <button>Click</button>
-                </Link> */}
-            </div>
-        </>
-    )
+        <Link to={"/productpage"}>
+        <button>See more</button> 
+        </Link>     
+    </section>
+  )
 }
 
 export default Productcard
--- src/component/Productlist.jsx
+++ src/component/Productlist.jsx
@@ -1,53 +1,56 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Productcard from './Productcard'
 
-const Productlist = ()=>{
+
+
+const Productlist = () => {
     const [list, setList] = useState([])
-    const [load, setLoad] = useState(false)
+    const [loading, setLoading] = useState(false)
 
     useEffect(()=>{
-        fetch()
-    }, [])
+        fetchAPI()
 
-    const fetch = async()=>{
-        setLoad(true)
+    },[])
 
-        try{
-            await fetch('https://fakestoreapi.com/products').then(async (response)=>{
+    const fetchAPI = async()=>{
+        setLoading(true)
+
+    try{
+        await fetch("https://fakestoreapi.com/products").then
+        (async (response)=>{
             setList(await response.json())
-            setLoad(false)}
-        )}
-    
-        catch{
-            (error)=>{
-                setLoad(false)
-                return console.log(error);
-            }
-        } 
+            setLoading(false)
+            
+        })
+
+    } catch{
+        (error)=>{
+            setLoading(false)
+            return console.log(error);
+        }
     }
 
-    return(
-    
-        <div className="parentProductlist">
-             {
-                     load ? <p>loading... </p> : 
-                     <div style={{display:'flex', gap: '30px', flexwrap: 'wrap'}}>
-                         { 
-                         list.map((info)=>{
+}
 
-                             return (
-                                     <Productcard key={info.id} title ={info.title} price ={info.price} description ={info.description} 
-                                     category ={info.category} image ={info.image} />
-                                 )
-                         })
-                         }
-                     </div>
-             }
 
-        </div>
-)
-
-} 
+  return (
+    <section>
+        <h1 style={{textAlign: 'center', marginBottom: '20px'}}>Up-to-date Wears & Gadgets</h1>
+        
+        {
+            loading ? <p style={{textAlign: 'center'}}>Loading...  <img style={{width: '70px', height: '70px', margin: 'center'}} src="./src/assets/loading.avif" alt="" /></p> :
+            <div style={{display: 'flex', gap: '30px', flexWrap: 'wrap'}}>
+                {
+                    list.map((data)=>{
+                        return(
+                            <Productcard key={data.id} title={data.title} price={data.price} description={data.description} category={data.category} image={data.image} />
+                        )
+                    })
+                }
+            </div>
+        }
+    </section>
+  )
+}
 
 export default Productlist
-
